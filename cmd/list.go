96883a793d2ded716d42058bae284efdfba706ee
Thomas Johansen <thomasjo@gmail.com>
2020-03-03 11:31:51 +0100

    Improve job list format

--- cmd/list.go
+++ cmd/list.go
@@ -3,50 +3,54 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 	"text/tabwriter"
+	"time"
 
+	"github.com/dustin/go-humanize"
+	"github.com/hako/durafmt"
 	"github.com/spf13/cobra"
 	batchv1 "k8s.io/api/batch/v1"
 )
 
 var (
-	headerNames = []string{
-		"NAME",
-		"SUCCEEDED",
+	showAll bool
+
+	listCmd = &cobra.Command{
+		Use:   "ls",
+		Short: "List jobs",
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			jobs, err := kubectx.ListJobs()
+			if err != nil {
+				return fmt.Errorf("could not list jobs: %w", err)
+			}
+
+			w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+			defer w.Flush()
+
+			fmt.Fprintln(w, header())
+			for _, job := range jobs.Items {
+				fmt.Fprintln(w, row(job))
+			}
+
+			return nil
+		},
 	}
 )
 
-// Flags
-var (
-	showAll bool
-)
-
-var listCmd = &cobra.Command{
-	Use:   "ls",
-	Short: "List jobs",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		jobs, err := kubectx.ListJobs()
-		if err != nil {
-			return fmt.Errorf("could not list jobs: %w", err)
-		}
-
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
-		defer w.Flush()
-
-		fmt.Fprintln(w, header())
-		for _, job := range jobs.Items {
-			fmt.Fprintln(w, row(job))
-		}
-
-		return nil
-	},
-}
-
 func init() {
-	listCmd.Flags().BoolVarP(&showAll, "all", "a", false, "show all jobs (defaults to only active)")
+	listCmd.Flags().BoolVarP(&showAll, "all", "a", false, "show all jobs; active and terminated")
 }
+
+var (
+	headerNames = []string{
+		"NAME",
+		"STATUS",
+		"DURATION",
+		"AGE",
+	}
+)
 
 func header() string {
 	return strings.Join(headerNames, "\t") + "\t" // TODO: Check if the trailing tab is required
@@ -65,9 +69,25 @@ func underlinedHeader() string {
 }
 
 func row(job batchv1.Job) string {
+	total := job.Status.Active + job.Status.Succeeded + job.Status.Failed
+	succeeded := job.Status.Succeeded
+
+	var start time.Time
+	duration := time.Duration(0)
+	if job.Status.StartTime != nil {
+		start = job.Status.StartTime.Time
+		end := time.Now()
+		if job.Status.CompletionTime != nil {
+			end = job.Status.CompletionTime.Time
+		}
+		duration = end.Sub(start)
+	}
+
 	data := []string{
 		job.Name,
-		strconv.Itoa(int(job.Status.Succeeded)),
+		fmt.Sprintf("%d/%d", succeeded, total),
+		durafmt.Parse(duration).LimitFirstN(2).String(),
+		humanize.Time(start),
 	}
 
 	return strings.Join(data, "\t") + "\t" // TODO: Check if the trailing tab is required
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/uitml/frink
 go 1.13
 
 require (
+	github.com/dustin/go-humanize v1.0.0
+	github.com/hako/durafmt v0.0.0-20191009132224-3f39dc1ed9f4
 	github.com/spf13/cobra v0.0.5
 	k8s.io/api v0.0.0-20191016110246-af539daaa43a
 	k8s.io/apimachinery v0.0.0-20191004115701-31ade1b30762
