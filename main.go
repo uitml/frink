3280dd5237772b48a694fbe635d02f3cca83b104
Thomas Johansen <thomasjo@gmail.com>
2020-07-17 14:20:24 +0200

    Add GoReleaser action

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "github.com/uitml/frink/cmd"
+import (
+	"github.com/uitml/frink/cmd"
+)
+
+var (
+	version string
+	commit  string
+	date    string
+)
 
 func main() {
 	cmd.Execute()
