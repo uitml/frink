5cd2e0c460822ddc0592b8fc3384fd76f209c832
Thomas Johansen <thomasjo@gmail.com>
2020-07-23 17:10:01 +0200

    docs: document exported mock things

--- internal/mocks/client.go
+++ internal/mocks/client.go
@@ -1,3 +1,5 @@
+// Package mocks contains utilities intended for mocking out functionality in test code.
+// Most notably, it contains mocks for interacting with the Kubernetes API.
 package mocks
 
 import (
@@ -6,10 +8,12 @@ import (
 	"k8s.io/client-go/rest"
 )
 
+// KubeClient is a mock k8s.KubeClient for interacting with a fake Kubernetes API, primarily intended for unit testing.
 type KubeClient struct {
 	Jobs []batchv1.Job
 }
 
+// ListJobs returns a list of jobs based on the Jobs field in KubeClient.
 func (client *KubeClient) ListJobs() (*batchv1.JobList, error) {
 	jobs := &batchv1.JobList{
 		Items: client.Jobs,
@@ -18,18 +22,22 @@ func (client *KubeClient) ListJobs() (*batchv1.JobList, error) {
 	return jobs, nil
 }
 
+// CreateJob simulates creating a job.
 func (client *KubeClient) CreateJob(job *batchv1.Job) error {
 	return nil
 }
 
+// DeleteJob simulates deleting a job.
 func (client *KubeClient) DeleteJob(name string) error {
 	return nil
 }
 
+// GetJob searches through the items in the Jobs field in KubeClient, returning the first item with a matching name.
 func (client *KubeClient) GetJob(name string) (*batchv1.Job, error) {
 	return nil, nil
 }
 
+// GetJobLogs simulates returning a rest.Request that will stream logs for a the job with the matching name.
 func (client *KubeClient) GetJobLogs(name string, opts *corev1.PodLogOptions) (*rest.Request, error) {
 	return nil, nil
 }
