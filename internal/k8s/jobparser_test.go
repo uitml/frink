2c61b61034968041c9eb9235da8a00941f5ba4b4
Thomas Johansen <thomasjo@gmail.com>
2020-08-25 17:01:40 +0200

    test: add tests for JobParser

---
+++ internal/k8s/jobparser_test.go
@@ -0,0 +1,56 @@
+package k8s
+
+import (
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseValidJobSpec(t *testing.T) {
+	fs := afero.NewBasePathFs(afero.NewOsFs(), "testdata")
+	parser := NewJobParser(fs)
+
+	job, err := parser.Parse("job/basic.yaml")
+	assert.NoError(t, err)
+	assert.Equal(t, "foo", job.Name)
+}
+
+func TestParseInvalidJobSpec(t *testing.T) {
+	fs := afero.NewBasePathFs(afero.NewOsFs(), "testdata")
+	parser := NewJobParser(fs)
+
+	job, err := parser.Parse("job/invalid.yaml")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "unknown field \"naem\"")
+	assert.Nil(t, job)
+}
+
+func TestParseValidSimpleJobSpec(t *testing.T) {
+	fs := afero.NewBasePathFs(afero.NewOsFs(), "testdata")
+	parser := NewJobParser(fs)
+
+	job, err := parser.Parse("simplejob/basic.yaml")
+	assert.NoError(t, err)
+	assert.Equal(t, "foo", job.Name)
+}
+
+func TestParseInvalidSimpleJobSpec(t *testing.T) {
+	fs := afero.NewBasePathFs(afero.NewOsFs(), "testdata")
+	parser := NewJobParser(fs)
+
+	job, err := parser.Parse("simplejob/invalid.yaml")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "unknown field \"naem\"")
+	assert.Nil(t, job)
+}
+
+func TestParseMissingFile(t *testing.T) {
+	fs := afero.NewBasePathFs(afero.NewOsFs(), "testdata")
+	parser := NewJobParser(fs)
+
+	job, err := parser.Parse("missing.yaml")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "no such file or directory")
+	assert.Nil(t, job)
+}
