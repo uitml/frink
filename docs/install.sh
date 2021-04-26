0f22be4d05b6c6f71c0523b19c239f6bc26190e1
Thomas Johansen <thomasjo@gmail.com>
2021-04-26 13:29:45 +0200

    Add arch tag to download URL

--- docs/install.sh
+++ docs/install.sh
@@ -10,9 +10,14 @@ case "${kernel}" in
   *) echo "unknown kernel" && exit 1 ;;
 esac
 
+arch="$(uname -m)"
+case "${arch}" in
+  x86_64*) arch="amd64" ;;
+esac
+
 api_url="https://raw.githubusercontent.com/uitml/frink/legacy"
 version="$(curl -s https://api.github.com/repos/uitml/frink/releases/latest | grep "\"name\": \"v.*\"" | cut -d '"' -f 4)"
-download_url="https://github.com/uitml/frink/releases/download/${version}/frink-${os}"
+download_url="https://github.com/uitml/frink/releases/download/${version}/frink-${os}-${arch}"
 
 echo ${download_url}
 
